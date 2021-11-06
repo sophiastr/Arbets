637f3563f144e6a7dc58da191aafcf72bd2e88cd
Meleniabebedeli <72795404+Meleniabebedeli@users.noreply.github.com>
2021-11-06 19:13:29 +0200

    Add files via upload

---
+++ mysql/data_.sql
@@ -0,0 +1,85 @@
+
+insert into sport values (1, "basketball", 2);
+insert into sport values(2, "football", 3);
+insert into sport values(3, "volleyball", 2);
+
+select * from team_game;
+
+alter table team drop column sports_club ;
+delete from team where team_id = 8;
+
+insert into team values (1, "Olympiakos", 1, "Sef");
+insert into team values (2, "Olympiakos", 2, "Karaiskaki");
+insert into team values (3, "Olympiakos", 3, "Melina Merkouri");
+insert into team values (4, "Panathinaikos", 1,"Nikos Galis" );
+insert into team values (5, "Panathinaikos", 2, "Apostolos Nikolaidis");
+insert into team values (6, "Panathinaikos", 3, "Makis Liougas");
+insert into team values (7, "Paok", 2, "Gipedo Toumpas");
+insert into team values (8, "Paok", 1, "Paok Sports Arena");
+insert into team values (9, "Paok", 3, "Paok Sports Arena");
+
+
+
+insert into game values (1, "2022-02-04 20:00:00","Karaiskaki","Olympiakos","Panathinaikos");
+insert into game values (2, "2022-02-04 22:00:00", "Nikos Galis","Panathinaikos","Olympiakos");
+insert into game values (3, "2022-02-05 21:30:00", "Sef" ,"Olympiakos","Paok");
+insert into game values (4, "2022-02-06 20:00:00", "Melina Merkouri","Olympiakos","Panathinaikos");
+insert into game values (5, "2022-02-07 20:00:00", "Makis Liougas","Panathinaikos", "Paok");
+insert into game values (6, "2022-02-07 22:30:00", "Sef", "Olympiakos","Panathinaikos");
+insert into game values (7, "2022-02-08 18:00:00", "Melina Merkouri" ,"Olympiakos","Paok");
+insert into game values (8, "2022-02-08 18:00:00", "Paok Sports Arena","Paok","Panathinaikos");
+insert into game values (9, "2022-02-08 18:00:00", "Sef" ,"Olympiakos","Panathinaikos");
+insert into game values (10, "2022-02-09 18:00:00", "Gipedo Toumpas", "Paok","Olympiakos");
+insert into game values (11, "2022-02-09 18:00:00", "Karaiskaki" ,"Olympiakos","Paok");
+insert into game values (12, "2022-02-09 18:00:00", "Paok Sports Arena" ,"Paok","Panathinaikos");
+insert into game values (13, "2022-02-10 18:00:00", "Paok Sports Arena" ,"Paok","Panathinaikos");
+insert into game values (14, "2022-02-10 18:00:00", "Makis Liougas" , "Panathinaikos","Paok");
+insert into game values (15, "2022-02-11 18:00:00", "Sef" ,"Olympiakos","Paok");
+insert into game values (16, "2022-02-11 18:00:00", "Gipedo Toumpas","Paok","Olympiakos");
+insert into game values (17, "2022-02-11 18:00:00", "Apostolos Nikolaidis" ,"Panathinaikos","Olympiakos");
+insert into game values (18, "2022-02-11 18:00:00", "Sef" ,"Olympiakos","Paok");
+insert into game values (19, "2022-02-12 20:00:00", "Karaiskaki" ,"Olympiakos","Paok");
+insert into game values (20, "2022-02-12 22:00:00", "Makis Liougas" , "Panathinaikos","Paok");
+
+
+insert into team_game values(2,1 );
+insert into team_game values(5,1 );
+insert into team_game values(4, 2);
+insert into team_game values(1, 2);
+insert into team_game values(1,3);
+insert into team_game values(8, 3);
+insert into team_game values(3, 4);
+insert into team_game values(6 ,4);
+insert into team_game values(6,5 );
+insert into team_game values(9,5 );
+insert into team_game values(1, 6);
+insert into team_game values(4, 6);
+insert into team_game values(3,7);
+insert into team_game values(9, 7);
+insert into team_game values(8, 8);
+insert into team_game values(4,8);
+insert into team_game values(1,9 );
+insert into team_game values(4, 9);
+insert into team_game values(7, 10);
+insert into team_game values(2,10);
+insert into team_game values(2, 11);
+insert into team_game values(7, 11);
+insert into team_game values(6,12);
+insert into team_game values(9,12);
+insert into team_game values(8,13);
+insert into team_game values(4, 13);
+insert into team_game values(6, 14);
+insert into team_game values(9,14);
+insert into team_game values(1, 15);
+insert into team_game values(8, 15);
+insert into team_game values(7,16);
+insert into team_game values(2,16);
+insert into team_game values(5,17);
+insert into team_game values(2, 17);
+insert into team_game values(1, 18);
+insert into team_game values(8,18);
+insert into team_game values(2, 19);
+insert into team_game values(7, 19);
+insert into team_game values(6,20);
+insert into team_game values(9,20);
+
